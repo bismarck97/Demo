5681e6d3859dd294cb2c16ea380365657734eaee
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-30 16:37:58 +0800

    海量用户通讯系统

--- Chatroom/client/login.go
+++ Chatroom/client/login.go
@@ -1,10 +1,69 @@
 package main
 
-import "fmt"
+import (
+	"Demo/Chatroom/common/message"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"net"
+)
 
 //写一个函数，完成登录校验
-func login(userID int, userPwd string) error {
+func login(userID int, userPwd string) (err error) {
 	//下一步就要开始定协议
-	fmt.Printf("userId = %d userPwd = %s\n", userID, userPwd)
-	return nil
+	//fmt.Printf("userId = %d userPwd = %s\n", userID, userPwd)
+	//return nil
+
+	//1.连接到服务器
+	conn, err := net.Dial("tcp", "localhost:8889")
+	if err != nil {
+		fmt.Println("net.Dial err:", err)
+		return
+	}
+	//延时关闭
+	defer conn.Close()
+
+	//2.准备通过conn发送消息给服务器
+	var mes message.Message
+	mes.Type = message.LoginMesType
+
+	//3.创建一个LoginMes 结构体
+	var loginMes message.LoginMes
+	loginMes.UserId = userId
+	loginMes.UserPwd = userPwd
+
+	//4.将loginMes序列化
+	data, err := json.Marshal(loginMes)
+	if err != nil {
+		fmt.Println("json.Marshal err:", err)
+		return
+	}
+
+	//5.把data赋给mes.Data
+	mes.Data = string(data)
+
+	//6.将mes进行序列化
+	data, err = json.Marshal(mes)
+	if err != nil {
+		fmt.Println("json.Marshal err:", err)
+		return
+	}
+
+	//7.到这个时候，data就是我们要发送的消息
+	//7.1 先把data的长度发送给服务器
+	//先获取到data的长度->转成一个表示长度的byte切片
+	var pkgLen uint32
+	pkgLen = uint32(len(data))
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[0:4], pkgLen)
+
+	//发送长度
+	n, err := conn.Write(buf[:])
+	if n != 4 || err != nil {
+		fmt.Println("conn.Write(bytes[:]) fail", err)
+		return
+	}
+	fmt.Println("客户端发送消息长度成功")
+
+	return
 }
---
+++ Chatroom/server/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"fmt"
+	"net"
+)
+
+func main() {
+	//提示信息
+	fmt.Println("服务器在8889端口监听...")
+	listen, err := net.Listen("tcp", "0.0.0.0:8889")
+	if err != nil {
+		fmt.Println("net.Listen err:", err)
+		return
+	}
+	defer listen.Close()
+	//一旦监听成功，就等到客户端来链接服务器
+	for {
+		fmt.Println("等待客户端来链接服务器")
+		conn, err := listen.Accept()
+		if err != nil {
+			fmt.Println("listen.Accept err:=", err)
+			continue
+		}
+		//一旦链接成功，则启动一个协程和客户端保持通讯
+		go process(conn)
+	}
+}
+
+//处理和客户端的通讯
+func process(conn net.Conn) {
+	//这里需要延时关闭conn
+	defer conn.Close()
+	//循环的读客户端发送的信息
+	for {
+		buf := make([]byte, 1024*4)
+		fmt.Println("等待读取...")
+		n, err := conn.Read(buf[:4])
+		if n != 4 || err != nil {
+			fmt.Println("conn.Read err:", err)
+			return
+		}
+		fmt.Println("读到的buf=", buf)
+	}
+}
