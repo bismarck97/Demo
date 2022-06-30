18fb23c784c2610cbd7a725e95639bd33f4f0cd9
bismarck <48767143+bismarck97@users.noreply.github.com>
2022-06-30 13:37:30 +0800

    海量用户通讯系统

--- Chatroom/client/login.go
+++ Chatroom/client/login.go
@@ -1,1 +1,10 @@
 package main
+
+import "fmt"
+
+//写一个函数，完成登录校验
+func login(userID int, userPwd string) error {
+	//下一步就要开始定协议
+	fmt.Printf("userId = %d userPwd = %s\n", userID, userPwd)
+	return nil
+}
--- Chatroom/client/main.go
+++ Chatroom/client/main.go
@@ -1,6 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+//定义两个变量，一个表示用户id 一个表示用户密码
+var userId int
+var userPwd string
 
 func main() {
 	//接收用户选择
@@ -23,7 +30,8 @@ func main() {
 			loop = false
 		case 3:
 			fmt.Println("退出系统")
-			loop = false
+			//loop = false
+			os.Exit(0)
 		default:
 			fmt.Println("输入有误，请重新输入")
 		}
@@ -31,8 +39,18 @@ func main() {
 	//根据用户的输入，显示新的提示信息
 	if key == 1 {
 		//说明用户要登录
-		fmt.Println("")
+		fmt.Println("请输入用户的ID")
+		fmt.Scanln(&userId)
+		fmt.Println("请输入用户的密码")
+		fmt.Scanln(&userPwd)
+		//先把登录的函数，写到另外一个文件，比如login.go
+		err := login(userId, userPwd)
+		if err != nil {
+			fmt.Println("登录失败")
+		} else {
+			fmt.Println("登录成功")
+		}
 	} else if key == 2 {
-
+		fmt.Println("进行用户注册逻辑")
 	}
 }
